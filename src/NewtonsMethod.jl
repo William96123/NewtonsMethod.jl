64e72f066c159cb95a41e7dac5ad40ad5d5c8cf3
WilliamD <duanbwilliam@gmail.com>
2022-02-15 21:24:28 -0800

    Added test for non convergence

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,5 +1,28 @@
 module NewtonsMethod
+export newtonroot
 
+using ForwardDiff
 # Write your package code here.
+function newtonroot(f, f_prime; x_0, tol = 1e-7, maxiter = 1000)
+    err = Inf
+    iter = 0
+    x = x_0
+    while err > tol && iter <= maxiter
+        iter = iter+1
+        xNew = x - f(x)/f_prime(x)
+        tol = abs(xNew - x)
+        x = xNew
+    end
+    if iter >= maxiter
+        return nothing
+    end
+    return x
+end
+
+function newtonroot(f; x_0, tol = 1e-7, maxiter = 1000)
+    f_prime = x -> ForwardDiff.derivative(f, x)
+    return newtonroot(f, f_prime; x_0 = x_0, tol = tol, maxiter = maxiter)
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,12 @@ using Test
 
 @testset "NewtonsMethod.jl" begin
     # Write your tests here.
+    @test 1 == 1
+end
+
+@testset "non convergence" begin
+    f(x) = 2 + x^2
+    f_prime(x) = 2x
+    println(newtonroot(f, f_prime; x_0 = 0))
+    @test newtonroot(f, f_prime; x_0 = 0) == nothing
 end
